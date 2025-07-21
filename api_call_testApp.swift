06d4b42552ce9eed698f4db93ae0fac16ab9b118
bacf <brunoagustincarusofassa@gmail.com>
2025-07-21 21:36:42 +0200

    Initial commit

---
+++ api_call_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_call_testApp.swift
+//  api-call-test
+//
+//  Created by Bruno Agustín Caruso Fassa on 21/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct api_call_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
