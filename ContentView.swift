f20b777f2ea9ef61973c43b0921315d285901ebf
bacf <brunoagustincarusofassa@gmail.com>
2025-07-23 13:50:53 +0200

    img data returning and showing in app

--- ContentView.swift
+++ ContentView.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var catImg: Cats?
+    @State private var catImg: URL?
 
     
     var body: some View {
         VStack(spacing: 20) {
-            Rectangle()
-                .foregroundColor(.secondary)
-                .frame(width: 120, height: 120)
+            
+            if let catImg = catImg {
+                AsyncImage(url: catImg) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
+                }
+                placeholder: {
+                    Rectangle()
+                        .foregroundColor(.secondary)
+                    
+                }
+                .frame(width: 200, height: 200)
+            }
+            
+            
             
             Text("Facts")
                 .bold()
@@ -26,38 +40,50 @@ struct ContentView: View {
             
             Spacer()
             
-            
-            
         }
         .padding()
         .task {
             do {
                 catImg = try await getImgCat()
+            } catch NetworkError.invalidUrl {
+                print("Invalid URL")
+            } catch NetworkError.invalidData {
+                print("Invalid data")
             } catch NetworkError.invalidResponse {
-                print("Invalid Response")
+                print("Invalid response")
+            } catch {
+                print("Unexpected error")
             }
         }
+       
         
     }
     
-    func getImgCat() async throws -> Cats {
-        guard let endpoint = URL(string: "https://api.thecatapi.com/v1/images/search") else { throw NetworkError.invalidUrl }
-        
-        let (data, response) = try await URLSession.shared.data(from: endpoint)
-        
-        guard let response = response as? HTTPURLResponse, response .statusCode == 200 else {
-            throw NetworkError.invalidResponse
+    func getImgCat() async throws -> URL {
+        guard let url = URL(string: "https://cataas.com/cat") else {
+            throw NetworkError.invalidUrl
         }
         
-        do {
-         let decoder = JSONDecoder()
+        return url
         
-            return try decoder.decode(Cats.self, from: data)
-        } catch {
-            throw NetworkError.invalidData
-        }
+//        var request = URLRequest(url: url)
+////        request.httpMethod = "GET"
+//        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+//        let (data, response) = try await URLSession.shared.data(from: url)
+        
+//        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+//            throw NetworkError.invalidResponse
+//        }
+//        
+//        do {
+//            let decoder = JSONDecoder()
+//            return try decoder.decode(Cats.self, from: data)
+//        } catch {
+//            throw NetworkError.invalidData
+//        }
     }
-
+//
 }
 
 #Preview {
@@ -66,9 +92,9 @@ struct ContentView: View {
 
 // ALL IN THE SAME FILE BECAUSE IM TESTING SOMETHING BRRUDA
 
-struct Cats: Decodable {
-    let url: String
-}
+//struct Cats: Decodable {
+//    let url: String
+//}
 
 enum NetworkError: Error {
     case invalidUrl
